6b59e2a7a1b867803b1b3434f7b1fe300b73bae8
Andrey Gud <nosacred@gmail.com>
2021-11-16 19:26:21 +0700

    more refactoring

--- main.c
+++ main.c
@@ -9,25 +9,10 @@
 #include <pulse/error.h>
 
 #define BUFFER_SIZE 1024 * 1000
+
 void error_handle(const char *message, int err) {
 		fprintf(stderr, "%s: %s\n", message, pa_strerror(err));
 }
-
-typedef struct {
-	uint32_t riff_heder;
-	uint32_t file_size;
-	uint32_t wave_header;
-	uint32_t fmt__header;
-	uint32_t size_of_wave_section_chunck;
-	uint16_t wave_type_format;
-	uint16_t mono_stereo;
-	uint32_t sample_rate;
-	uint32_t bytes_sec;
-	uint16_t block_aligment;
-	uint16_t bit_sample;
-	uint32_t data_description_header;
-	uint32_t size_of_data_chunck;
-} Header;
 
 #define FILE_NAME "./speech.wav"
 
@@ -59,14 +44,6 @@ int main(void)
 	ss.channels = *((char*)(file_buffer + 22));
 	ss.rate = *((int*)(file_buffer + 24));
 	printf("Number of channels: %d, Sample rate %d.\n", ss.channels, ss.rate);
-
-	/*s = pa_simple_new(NULL, "puple", PA_STREAM_RECORD,
-					NULL, "Music", &ss, NULL, NULL, NULL);
-	assert(s && "PulseAudio connection");
-
-	if(pa_simple_read(s, (void*)audio_buffer, audio_buffer_size, &err))
-		error_handle("Read fail", err);
-	pa_simple_free(s); */
 
 	s = pa_simple_new(NULL, "puple", PA_STREAM_PLAYBACK,
 					NULL, "Music", &ss, NULL, NULL, NULL);
