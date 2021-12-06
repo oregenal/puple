0ede928af92262e56b427cfacab1ef6c448ece22
Andrey Gud <nosacred@gmail.com>
2021-12-06 19:15:08 +0700

    break

--- src/mp3.c
+++ src/mp3.c
@@ -1,8 +1,41 @@
 #include "mp3.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 
 void play_mp3_file(const char *file_name)
 {
+	struct stat file_stat;
+	if(stat(file_name, &file_stat)) {
+		perror("File error");
+		return;
+	}
+
+	if(!S_ISREG(file_stat.st_mode)) {
+		fprintf(stderr, "Not a file.\n");
+		return;
+	}
+
+	FILE *audio_file = fopen(file_name, "rb");
+	if(!audio_file) {
+		perror("File open error.");
+		return;
+	}
+	char *file_buffer = malloc(sizeof(char) *file_stat.st_size);
+	if(fread((void*)file_buffer, 1, file_stat.st_size, audio_file) 
+			!= file_stat.st_size) {
+		fprintf(stderr, "File read error.\n");
+		return;
+	}
+
+	if(strncmp(file_buffer, "ID3", 3) != 0 ) {
+		fprintf(stderr, "Unsupported file format.\n");
+		return;
+	}
+
 	printf("Not implemented.\n");
+
+	free(audio_file);
 }
