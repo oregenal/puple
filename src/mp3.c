5a3ed8185beb4c48ee1b2c4c7dc900a6914e7066
Andrey Gud <nosacred@gmail.com>
2021-12-09 13:40:09 +0700

    main loop

--- src/mp3.c
+++ src/mp3.c
@@ -8,7 +8,62 @@
 #include <string.h>
 #include <sys/stat.h>
 
-static const unsigned int SYNC = 0xfff0; /* 1111 1111 1111b */
+static const unsigned int SYNCWORD = 0xfff0; /* 1111 1111 1111b */
+
+static void play_MPEG_1()
+{
+	printf("MPEG-1\n");
+}
+
+static void play_MPEG_2()
+{
+	printf("MPEG-2\n");
+}
+
+static void get_info(const char *file_buffer, int frame_position)
+{
+	printf("SYNCWORD: %d.\n", frame_position);
+
+	switch(*(int*)(file_buffer + frame_position + 3) & 0x8) {
+		case 0:
+			play_MPEG_2();
+			break;
+		case 0x8:
+			play_MPEG_1();
+			break;
+		default: {}
+	}
+
+	switch(*(int*)(file_buffer + frame_position + 3) & 0x6) {
+		case 0:
+			printf("Layer not defined.\n");
+			break;
+		case 0x2:
+			printf("Layer III.\n");
+			break;
+		case 0x4:
+			printf("Layer II.\n");
+			break;
+		case 0x6:
+			printf("Layer I.\n");
+			break;
+		default: {}
+	}
+
+	switch(*(int*)(file_buffer + frame_position + 5) & 0xc) {
+		case 0:
+			printf("22050\n");
+			break;
+		case 0x4:
+			printf("24000\n");
+			break;
+		case 0x8:
+			printf("16000\n");
+			break;
+		default: {}
+	}
+}
+
 
 void play_mp3_file(const char *file_name)
 {
@@ -40,49 +95,18 @@ void play_mp3_file(const char *file_name)
 		return;
 	}
 
-	int frame = str_search_ptrn((char*)&SYNC, file_buffer, file_stat.st_size);
-
-	switch(*(int*)(file_buffer + frame + 3) & 0x8) {
-		case 0:
-			printf("MPEG-2\n");
-			break;
-		case 0x8:
-			printf("MPEG-1\n");
-			break;
-		default: {}
+	int frame_position = 0, frame_start = 0;
+	while((frame_start = str_search_ptrn((char*)&SYNCWORD, 
+					file_buffer + frame_position + frame_start, 
+					file_stat.st_size - frame_position)) > 0) {
+		frame_position += frame_start;
+		get_info(file_buffer, frame_position);
+		frame_position += 32;
+		frame_start = 0;
 	}
 
-	switch(*(int*)(file_buffer + frame + 3) & 0x6) {
-		case 0:
-			printf("Layer not defined.\n");
-			break;
-		case 0x2:
-			printf("Layer III.\n");
-			break;
-		case 0x4:
-			printf("Layer II.\n");
-			break;
-		case 0x6:
-			printf("Layer I.\n");
-			break;
-		default: {}
-	}
-
-	switch(*(int*)(file_buffer + frame + 5) & 0xc) {
-		case 0:
-			printf("22050\n");
-			break;
-		case 0x4:
-			printf("24000\n");
-			break;
-		case 0x8:
-			printf("16000\n");
-			break;
-		default: {}
-	}
-
-	printf("SYNC: %d.\n", frame);
 	printf("Not implemented.\n");
 
-	free(audio_file);
+	free(file_buffer);
+	fclose(audio_file);
 }
