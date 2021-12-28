8de148948906933ec3c0c34d8668c8a57bb3f40d
Andrey Gud <nosacred@gmail.com>
2021-12-28 11:27:48 +0700

    unpack data wich is in same frame

--- src/play_frame.c
+++ src/play_frame.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <string.h>
+
 #include "play_frame.h"
 
 static const uint8_t slen[2][16] = 
@@ -6,7 +9,22 @@ static const uint8_t slen[2][16] =
 
 void play_frame(const char *file_buffer, frame_t *frame_props)
 {
+	unsigned char *main_data;
 	int channels = frame_props->channel_mode == SINGLE_CHANNEL ? 1 : 2;
+
+	int header_len = 4 + (channels == 1 ? 17 : 32);
+	header_len += frame_props->protection_bit == PROTECTED_BY_CRC ? 2 : 0;
+
+	/* Unpack main data */
+	if(frame_props->main_data_begin == 0) {
+		/* If main_data_begin = 0, main data started right after heade +
+		 * side_info + crc if set */
+		main_data = malloc(frame_props->length - header_len);
+		memcpy(main_data, file_buffer + frame_props->location + header_len, 
+				frame_props->length - header_len);
+	} else {
+		main_data = malloc(frame_props->length - header_len);
+	}
 
 	for(int gr = 0; gr < 2; ++gr)
 		for(int ch = 0; ch < channels; ++ch) {
@@ -14,10 +32,10 @@ void play_frame(const char *file_buffer, frame_t *frame_props)
 			uint8_t scf_com = frame_props->scalefac_compress[gr][ch];
 
 			/* Decoding scalefactors */
-			if((frame_props->block_type[gr][ch] = 0x2) 
+			if((frame_props->block_type[gr][ch] == 0x2) 
 					&& (frame_props->mixed_block_flag[gr][ch] == 0))
 				part2_length = 18 * slen[1][scf_com] + 18 * slen[2][scf_com];
-			else if((frame_props->block_type[gr][ch] = 0x2) 
+			else if((frame_props->block_type[gr][ch] == 0x2) 
 					&& (frame_props->mixed_block_flag[gr][ch] == 1))
 				part2_length = 17 * slen[1][scf_com] + 18 * slen[2][scf_com];
 			else 
@@ -26,4 +44,6 @@ void play_frame(const char *file_buffer, frame_t *frame_props)
 			/* TODO: remove dummy part2_length */
 			(void)part2_length;
 		}
+
+	free(main_data);
 }
